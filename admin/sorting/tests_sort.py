a86c9a689d89b88f9396cf89bd3375e614832884
Younyeojin <yjyoon2107@gmail.com>
2021-09-27 09:53:52 +0900

    0927

--- admin/sorting/tests_sort.py
+++ admin/sorting/tests_sort.py
@@ -16,9 +16,9 @@ class TestSorting(unittest.TestCase):
         self.assertEqual(arr, [1, 2, 3, 4, 5, 6, 7, 8, 9])
 
     def test_quick_sort(self):
-        param = [9, 8, 7, 6, 5, 3, 2, 4, 1]
-        # arr = [99, 8, 17, 6, 51, 3, 21, 4, 1, 2, 5, 9]
-        arr1 = Sorting.quick_sort(param)
-        # arr2 = Sorting.quick_sort(arr)
-        self.assertEqual(arr1, [1, 2, 3, 4, 5, 6, 7, 8, 9])
-        print(arr1)
+        # param = [9, 8, 7, 6, 5, 3, 2, 4, 1]
+        arr = [99, 8, 17, 6, 51, 3, 21, 4, 1, 2, 5, 9]
+        # arr1 = Sorting.quick_sort(param)
+        arr2 = Sorting.quick_sort(arr)
+        self.assertEqual(arr2, [1, 2, 3, 4, 5, 6, 8, 9, 17, 21, 51, 99])
+        print(arr2)
